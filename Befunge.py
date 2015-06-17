34b6679102eb6628a609961eed60facf251c8576
hppavilion1 <nathantronnes@gmail.com>
2015-06-16 19:16:25 -0800

    Moved functions back to main loop

--- Befunge.py
+++ Befunge.py
@@ -5,110 +5,117 @@ from BefungeLexer import lex #The lexer function
 from BefungeHelpers import *
 
 class IP:
-    def __init__(self, pos, self.delta):
+    def __init__(self, pos, delta):
         global direcs
         sm = False #Is in string mode?
         self.state = None
         self.pos = pos #Instruction Pointer Position
-        self.delta = delta #Instruction Pointer self.delta (N|NE|E|SE|S|SW|W|NW)
-        self.vector = direcs[self.delta] #Vector mapped to the self.delta
+        self.delta = delta #Instruction Pointer delta (N|NE|E|SE|S|SW|W|NW)
+        self.vector = direcs[self.delta] #Vector mapped to the delta
 
-    def __call__(self, ins):
-        global direcs
-        if sm: #Only applies in string mode
-            if ins == '>': #Go East
-                self.delta = 'E'
+def execute(script): #Execute a lexed script
+    global direcs
+    pointers = [IP((0, 0), 'E')]
+    s = stack()
 
-            elif ins == '<': #Go West
-                self.delta = 'W'
+    while len(pointers)>0:
+        for pointer in pointers: #Iterate through pointers epointerecuting commands
+            if sm: #Only applies in string mode
+                if ins == '>': #Go East
+                    pointer.delta = 'E'
 
-            elif ins == '^': #Go North
-                self.delta = 'N'
+                elif ins == '<': #Go West
+                    pointer.delta = 'W'
 
-            elif ins == 'v': #Go South
-                self.delta = 'S'
+                elif ins == '^': #Go North
+                    pointer.delta = 'N'
 
-            elif ins == '?': #Go Away
-                self.delta = choice(direcs.keys())
+                elif ins == 'v': #Go South
+                    pointer.delta = 'S'
 
-            elif ins == ']': #Turn Right
-                if self.delta == 'N':
-                    self.delta = 'E'
-                elif self.delta == 'NE':
-                    self.delta = 'SE'
-                elif self.delta == 'E':
-                    self.delta = 'S'
-                elif self.delta == 'SE':
-                    self.delta = 'SW'
-                elif self.delta == 'S':
-                    self.delta = 'W'
-                elif self.delta == 'SW':
-                    self.delta = 'NW'
-                elif self.delta == 'W':
-                    self.delta = 'N'
-                elif self.delta == 'NW':
-                    self.delta = 'NE'
+                elif ins == '?': #Go Away
+                    pointer.delta = choice(direcs.keys())
 
-            elif ins == '[': #Turn Left
-                if self.delta == 'N':
-                    self.delta = 'W'
-                elif self.delta == 'NE':
-                    self.delta = 'NW'
-                elif self.delta == 'E':
-                    self.delta = 'N'
-                elif self.delta == 'SE':
-                    self.delta = 'NE'
-                elif self.delta == 'S':
-                    self.delta = 'E'
-                elif self.delta == 'SW':
-                    self.delta = 'SE'
-                elif self.delta == 'W':
-                    self.delta = 'S'
-                elif self.delta == 'NW':
-                    self.delta = 'SW'
+                elif ins == ']': #Turn Right
+                    if pointer.delta == 'N':
+                        pointer.delta = 'E'
+                    elif pointer.delta == 'NE':
+                        pointer.delta = 'SE'
+                    elif pointer.delta == 'E':
+                        pointer.delta = 'S'
+                    elif pointer.delta == 'SE':
+                        pointer.delta = 'SW'
+                    elif pointer.delta == 'S':
+                        pointer.delta = 'W'
+                    elif pointer.delta == 'SW':
+                        pointer.delta = 'NW'
+                    elif pointer.delta == 'W':
+                        pointer.delta = 'N'
+                    elif pointer.delta == 'NW':
+                        pointer.delta = 'NE'
 
-            elif ins == 'r': #Reflect
-                self.delta = value2key(direcs, (direcs[self.delta][0]*-1, direcs[self.delta][1]*-1))
+                elif ins == '[': #Turn Left
+                    if pointer.delta == 'N':
+                        pointer.delta = 'W'
+                    elif pointer.delta == 'NE':
+                        pointer.delta = 'NW'
+                    elif pointer.delta == 'E':
+                        pointer.delta = 'N'
+                    elif pointer.delta == 'SE':
+                        pointer.delta = 'NE'
+                    elif pointer.delta == 'S':
+                        pointer.delta = 'E'
+                    elif pointer.delta == 'SW':
+                        pointer.delta = 'SE'
+                    elif pointer.delta == 'W':
+                        pointer.delta = 'S'
+                    elif pointer.delta == 'NW':
+                        pointer.delta = 'SW'
 
-            elif ins == 'x': #Absolute Vector
-                dy = s.pop()
-                dx = s.pop()
+                elif ins == 'r': #Reflect
+                    pointer.delta = value2key(direcs, (direcs[pointer.delta][0]*-1, direcs[pointer.delta][1]*-1))
 
-                self.delta = value2key(direcs, (dx, dy))
+                elif ins == 'pointer': #Absolute Vector
+                    dy = s.pop()
+                    dx = s.pop()
 
-            elif ins == '#': #Trampline
-                loc[0] += self.vector[0]
-                loc[1] += self.vector[1]
+                    pointer.delta = value2key(direcs, (dx, dy))
 
-            elif ins == '@': #Stop instruction
-                self.delta = None
+                elif ins == '#': #Trampoline
+                    loc[0] += self.vector[0]
+                    loc[1] += self.vector[1]
+
+                elif ins == '@': #Stop
+                    pointer.delta = None
+
+                elif ins == ';': #Jump Over (to be implemented)
+                    pass
+
+                elif ins == 'j'
             
         else:
             pass
 
         if self.delta in direcs:
-            self.vector = direcs[self.delta]
+            pointer.vector = direcs[pointer.delta]
             
-            loc[0] += self.vector[0]
-            loc[1] += self.vector[1]
+            pointer.loc[0] += pointer.vector[0]
+            pointer.loc[1] += pointer.vector[1]
         else:
-            raise BefungeError('Invalid self.delta')
+            raise BefungeError('Invalid delta')
 
-        if self.vector[0] == 0 and self.vector[1] == 0:
-            self.state = 'stopped'
+        if pointer.vector[0] == 0 and pointer.vector[1] == 0:
+            pointer.state = 'stopped'
             
-        elif self.vector[0] == 0 or self.vector[1] == 0:
-            self.state = 'moving'
+        elif pointer.vector[0] == 0 or pointer.vector[1] == 0:
+            pointer.state = 'moving'
 
         else:
-            self.state = 'flying'
-
-def execute(script): #Execute a lexed script
-    s = stack()
-
-    while len(pointers)>0:
-        for x in pointers:
-            x(script[x.pos[0]][x.pos[1]])
+            pointer.state = 'flying'
+            
+        for pointer in range(len(pointers)): #Delete stopped pointers
+            if pointers[pionter].state == 'stopped': 
+                del pointers[x]
 
 def interpret(script):
     return(execute(lex(script)))
