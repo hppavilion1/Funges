bdbb8e2bfdb3072e7567d32b04b17685550eddc3
hppavilion1 <nathantronnes@gmail.com>
2015-06-16 17:45:44 -0800

    Merged files into interpreter

--- BefungeInterpreter.py
+++ BefungeInterpreter.py
@@ -1,5 +1,13 @@
-from BefungeLexer import lex
-from BefungeExecuter import execute
+from stack import stack
+
+def lex(script):
+    script=script.split('\n')
+    for x in range(len(script)):
+        script[x]=[y for y in script[x]]
+    return(script)
+
+def execute(script):
+    s=stack()
 
 def interpret(script):
     return(execute(lex(script)))
--- BefungeLexer.py
+++
@@ -1,5 +0,0 @@
-def lex(script):
-    script=script.split('\n')
-    for x in range(len(script)):
-        script[x]=[y for y in script[x]]
-    return(script)
